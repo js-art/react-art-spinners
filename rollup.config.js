93afd69d6ca2d9008f86c0cf61511623b4aa3df9
Mostafa Naderi <javascript.github@gmail.com>
2019-09-30 12:52:26 +0330

    working on rimraf and rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+const dist = 'dist'
+export default {
+    input: 'src/index.js',
+    output: [
+        {
+            file: `${dist}/bundle.cjs.js`,
+            format: 'cjs'
+        },
+        {
+            file: `${dist}/bundle.esm.js`,
+            format: 'esm'
+        },
+        {
+            name: 'reactArtSpinners',
+            file: `${dist}/bundle.umd.js`,
+            format: 'umd'
+        }
+    ]
+}
